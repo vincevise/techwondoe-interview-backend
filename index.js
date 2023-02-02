4ffc0b4a12f7468327156ddb5811dbb376cf71ae
vincevise <vincentvuram204@gmail.com>
2023-02-02 18:06:40 +0530

    added a random route

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
-const path = require('path')
 
 const server = jsonServer.create()
 const router = jsonServer.router( 'db.json')
-console.log(__dirname)
 const middlewares = jsonServer.defaults()
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
+server.use('/api',(req,res)=>{
+  try{
+    res.status(200).json({data:'hogaya'})
+  }catch(error){
+    res.status(400).json(error.message)
+  }
+})
 server.use(router)
 
-const PORT = 3023
+const PORT = 3026
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT,__dirname}`)
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
